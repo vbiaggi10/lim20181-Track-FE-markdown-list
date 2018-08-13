8c82aaadf3d46458940e14fc671a6f9176e53cc7
vbiaggi10 <vbiaggi10@gmail.com>
2018-08-13 16:29:35 -0500

    Closes #4

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
-import lodash from 'lodash';
+/* import lodash from 'lodash';
 exports.printMsg = function() {
     console.log('This msg is from boilerplate');
-}
+} */
 
-const mdLinks = require("md-links");
+const fs = require("fs");
+
+fs.readFile('./src/index.md', (err, res) => {
+  if (/hola/.test(res.toString()))
+    console.log("si se encontro")
+  else
+    console.log("No se encontro")
+});
+
+
+
+
+
+/* const mdLinks = require("md-links");
 
 mdLinks("./some/example.md")
   .then(links => {
@@ -12,16 +25,16 @@ mdLinks("./some/example.md")
   .catch(console.error);
 
 mdLinks("./some/example.md", {
-    validate: true
-  })
+  validate: true
+})
   .then(links => {
     // => [{ href, text, file, status, ok }]
   })
   .catch(console.error);
 
 mdLinks("./some/example.md", {
-    stats: true
-  })
+  stats: true
+})
   .then(links => {
     // => [{ href, text, file, total, unique, domains }]
   })
@@ -31,4 +44,4 @@ mdLinks("./some/dir")
   .then(links => {
     // => [{ href, text, file }]
   })
-  .catch(console.error);
+  .catch(console.error); */
