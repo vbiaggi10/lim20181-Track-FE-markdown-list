52d454cd32075341a69cd9f02d1a582ffa07f70f
vbiaggi10 <vbiaggi10@gmail.com>
2018-08-13 17:01:24 -0500

    Closes #3

--- index.js
+++ index.js
@@ -4,13 +4,51 @@ exports.printMsg = function() {
 } */
 
 const fs = require("fs");
+var path = require('path');
+let file;
 
-fs.readFile('./src/index.md', (err, res) => {
-  if (/hola/.test(res.toString()))
-    console.log("si se encontro")
-  else
-    console.log("No se encontro")
+process.argv.forEach((val, index) => {
+  if (index === 2) {
+    file = val;
+    var path_splitted = file.split('.');
+    var extension = path_splitted.pop();
+    if ( extension === 'md'){
+      fs.readFile(file, (err, res) => {
+        if (/hola/.test(res.toString()))
+          console.log("si se encontro")
+        else
+          console.log("No se encontro")
+      });
+    }
+  }
+})
+/* var dir = './';
+fs.readdir(dir, (err, files) => {
+  var r = [];
+  files.forEach((file) => {
+    s(file);
+    function s(file) {
+      fs.stat(dir + '/' + file, (err, stat) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        if (stat.isDirectory())
+          r.push({ f: file, type: 'dir' });
+        else if (stat.isFile())
+          r.push({ f: file, type: 'file' });
+        else 
+          r.push(0);
+        if (r.length == files.length) {
+          r.filter((m) => { return m; });
+          console.log(r);
+        }
+      });
+    }
+  });
 });
+ */
+
 
 
 
