9b507a3eb1a4a163a419a96932b9ebbef7b85e7e
vbiaggi10 <vbiaggi10@gmail.com>
2018-08-20 18:30:40 -0500

    refactorizando app

--- app.js
+++ app.js
@@ -4,10 +4,17 @@ const fs = require('fs');
 const fetch = require('node-fetch');
 const path = require('path');
 
+let link = {
+  href: '',
+  text: '',
+  file: ''
+};
+
 let options = {
   validate: false,
   stats: false
 };
+
 let totalCount = 0;
 let okCount = 0;
 let brokenCount = 0;
@@ -41,48 +48,47 @@ const readDirectory = (dir, done) => {
 const readFileMD = (fileMD, done) => {
   let results = [];
   fs.readFile(fileMD, (err, list) => {
-    list = list.toString().split("\n");
-
+    list = list.toString().split(' ');
     if (err) return done(err);
     var pending = list.length;
     if (!pending) return done(null, results);
-    list.forEach((url) => {
-      console.log(url)
-      const expressionLink = /[^()]((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
-      const expressionLinkMD = /\[([\w\s]*)\]\(((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)\)/gi;
-      const expressionLinkMatch = url.match(expressionLink);
-      const expressionLinkMDMatch = url.match(expressionLinkMD);
-      if (expressionLink.test(expressionLinkMatch)) {
-        const newUrl = url.replace(/[\n \ ]/gi, '');
-        console.log(newUrl)
 
-        results.push(newUrl);
+    const expressionLink = /[^()]((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
+    const expressionLinkMD = /\[([\w\s]*)\]\(((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)\)/gi;
+
+    if (expressionLink.test(results)) {
+      readFileMD(list, (err, res) => {
+        console.log(res)
         if (!--pending) done(null, results);
-        // console.log(fileMD + '  \t' + newUrl + '  \tLink a  \t' + tittle);
-
-      }
-      // fs.stat(url, (err, stat) => {
-      //   if (stat && stat.isDirectory()) {
-      //     readFileMD(url, (err, res) => {
-      //       results = results.concat(res);
-      //       if (!--pending) done(null, results);
-      //     });
-      //   } else {
-      //     results.push(url);
-      //     // results = results.filter(checkMD);
-      //     // readFileMD(results, options)
-      //     if (!--pending) done(null, results);
-      //   }
-      // });
-
-    });
+      });
+    } else if (expressionLinkMD.test(results)) {
+      results.push(list);
+      console.log(results)
+      if (!--pending) done(null, results);
+    }
   });
 
 
 };
 
 const checkMD = (results) => {
-  return path.extname(results) === '.md'
+  return path.extname(results) === '.md';
+}
+
+const replaceURL = (results) => {
+  const expressionLink = /[^()]((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)/gi;
+  const expressionLinkMD = /\[([\w\s]*)\]\(((ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?)\)/gi;
+
+  // const expressionLinkMatch = results.match(expressionLink);
+  // const expressionLinkMDMatch = results.match(expressionLinkMD);
+
+  if (expressionLink.test(results)) {
+    console.log('89: ' + results)
+  } else if (expressionLinkMD.test(results)) {
+    console.log('91: ' + results)
+  } else {
+    console.log('no')
+  }
 }
 
 readFileMD('./src/index.md', function (err, results) {
